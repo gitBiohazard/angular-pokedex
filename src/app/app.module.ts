9187aef1cfbafded4adf086a305c4dacf304c526
Luiz Felipe Pedrosa Cavalcante <54713427+gitBiohazard@users.noreply.github.com>
2022-09-28 22:18:56 -0300

    new: page scss footer/header/search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PokemonHeaderComponent } from './pokemon-header/pokemon-header.component';
+import { PokemonFooterComponent } from './pokemon-footer/pokemon-footer.component';
+import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
-    PokemonCardComponent
+    PokemonCardComponent,
+    PokemonHeaderComponent,
+    PokemonFooterComponent,
+    PokemonSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pokemon-footer/pokemon-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-footer',
+  templateUrl: './pokemon-footer.component.html',
+  styleUrls: ['./pokemon-footer.component.scss']
+})
+export class PokemonFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
