b7c437e24943b3b88449cf28dd03b337193c1c9a
Luiz Felipe Pedrosa Cavalcante <54713427+gitBiohazard@users.noreply.github.com>
2022-10-01 01:00:18 -0300

    update: unsubscribe

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,23 +1,26 @@
 import { PokemonSearchService } from './../pokemon-search/pokemon-search.service';
 import { PokemonService } from './../pokemon/pokemon.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, OnDestroy {
   constructor(
     public pokemonService: PokemonService,
     public PokemonSearchService: PokemonSearchService
-    ) { }
+  ) { }
 
+
+  private subscription: Subscription = new Subscription()
   private setPokemonsResponse: any
   public pokemonsResponse: any
 
   ngOnInit(): void {
-    this.pokemonService.apiListAllPokemons.subscribe(res => {
+    this.subscription = this.pokemonService.apiListAllPokemons.subscribe(res => {
       this.setPokemonsResponse = res.results
       this.pokemonsResponse = this.setPokemonsResponse
     })
@@ -27,8 +30,12 @@ export class PokemonListComponent implements OnInit {
     })
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
   public getSearch(value: string): void {
-    const filter = this.setPokemonsResponse.filter((res: any) => {
+      const filter = this.setPokemonsResponse.filter((res: any) => {
       return !res.name.indexOf(value.toLowerCase())
     })
     this.pokemonsResponse = filter
